4a9ce499e4e5553123bc7bff3046f60e4eebbd6b
vlad <qas22042007@gmail.com>
2025-01-04 03:26:56 +0300

    create skeleton

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Rubic</title>
+</head>
+<body>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/three.js"></script>
+    <script src="js/index.js"></script>
+</body>
+</html>
